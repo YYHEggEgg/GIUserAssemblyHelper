ec3b4c9cfe45792104e1c292d7c95eb0a6f61b12
EggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2022-12-23 18:58:38 +0800

    v1.0.0 - Initial

---
+++ GIUserAssemblyHelper/Program.cs
@@ -0,0 +1,48 @@
+﻿using YYHEggEgg.HexAPI;
+using YYHEggEgg.HexAPI.OutputHelper;
+using System.Diagnostics;
+using System.Text;
+
+namespace GIUserAssemblyHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the UserAssembly.dll path");
+            string path = Console.ReadLine();
+
+            FileHexObject fileHexObject = new FileHexObject(path, false, 102400, 1);
+
+            foreach (var index in fileHexObject.Find(Encoding.Default.GetBytes("<RSAKeyV")))
+            {
+                Console.WriteLine($"Found: {HexHelper.GetHexFormat(index)}");
+
+                string output = "";
+                for (Int64 i = index; !output.Contains("</RSAKeyValue>"); )
+                {
+                    // Read 8 bytes
+                    output += Encoding.Default.GetString(fileHexObject.GetRange(i, 8));
+                    i += 8;
+                    // The encoded (or interferent) string starts with H (0x48), and ends with º (0xBA)
+                    Debug.Assert(fileHexObject[i] == 0x48);
+                    for (Int64 j = i; j < fileHexObject.Length; j++)
+                    {
+                        if (fileHexObject[j] == 0xBA)
+                        {
+                            i = j + 1;
+                            break;
+                        }
+                    }
+                }
+
+                // Cut off invisible chars at the end
+                int end = output.LastIndexOf("</RSAKeyValue>") + 14;
+                output = output.Substring(0, end);
+                Console.WriteLine(output);
+            }
+
+             Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GIUserAssemblyHelper
-UA helper for the anime game: `Go: Impossible`
+UA helper for the anime game `Go: Impossible`.
+
+Note: This program doesn't have write feature, because it's useless after the 33.0 version of the certain anime game.
+
+I suggest you to read [this tutorial](https://github.com/YYHEggEgg/GIUserAssemblyHelper/blob/main/Tutorial.md) to understand what the project is doing. [Chinese version](https://github.com/YYHEggEgg/GIUserAssemblyHelper/blob/main/Tutorial_CN.md)
+
+## Developing
+For compiling, please download the [HexAPI](https://github.com/YYHEggEgg/HexAPI) and set the .csproj file as a reference. 
+
+You can also directly refer to the released [HexAPI.dll](https://github.com/YYHEggEgg/HexAPI/releases) file.
